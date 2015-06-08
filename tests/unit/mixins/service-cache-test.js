a08e21fb52b0c46b8b90221719fb54c56f06d317
Bill Heaton <pixelhandler@gmail.com>
2015-06-08 10:31:20 -0700

    Add unit tests for service cache mixin

--- tests/unit/mixins/service-cache-test.js
+++ tests/unit/mixins/service-cache-test.js
@@ -1,20 +1,54 @@
 import Ember from 'ember';
 import ServiceCacheMixin from '../../../mixins/service-cache';
 import { module, test } from 'qunit';
+import { Post } from 'dummy/tests/helpers/resources';
 
-module('Unit | Mixin | service cache');
+let sandbox, subject;
 
-// Replace this with your real tests.
-test('it works', function(assert) {
-  var ServiceCacheObject = Ember.Object.extend(ServiceCacheMixin);
-  var subject = ServiceCacheObject.create();
-  assert.ok(subject);
+module('Unit | Mixin | service cache', {
+  beforeEach() {
+    sandbox = window.sinon.sandbox.create();
+    let ServiceCacheObject = Ember.Object.extend(ServiceCacheMixin);
+    subject = ServiceCacheObject.create();
+  },
+  afterEach() {
+    sandbox.restore();
+  }
 });
 
-QUnit.skip('#initCache', function(assert) {});
+test('#initCache', function(assert) {
+  assert.ok(subject.cache, 'cache property setup');
+  assert.equal(subject.cache.meta, null, 'cache meta property setup as null');
+  assert.ok(Array.isArray(subject.cache.data.get('content')), 'cache data property setup as Array');
+});
 
-QUnit.skip('#cacheResource', function(assert) {});
+test('#cacheResource calls #cacheMeta and #cacheData', function(assert) {
+  sandbox.stub(subject, 'cacheMeta', function () { return; });
+  sandbox.stub(subject, 'cacheData', function () { return; });
+  let resource = {};
+  subject.cacheResource(resource);
+  assert.ok(subject.cacheMeta.calledOnce, 'cacheMeta called');
+  assert.ok(subject.cacheMeta.calledWith(resource), 'cacheMeta called with resource');
+  assert.ok(subject.cacheData.calledWith(resource), 'cacheData called with resource');
+  assert.ok(subject.cacheData.calledOnce, 'cacheData called');
+});
 
-QUnit.skip('#cacheMeta', function(assert) {});
+test('#cacheMeta', function(assert) {
+  let resource = { meta: { total: 5 } };
+  subject.cacheMeta(resource);
+  assert.equal(subject.get('cache.meta'), resource.meta, 'meta object set on cache.meta');
+});
 
-QUnit.skip('#cacheData', function(assert) {});
+test('#cacheData stores a single resource', function(assert) {
+  let resource = Post.create({ id: '1' });
+  subject.cacheData({ data: resource });
+  assert.equal(subject.get('cache.data').get('firstObject'), resource, 'resource added to cache.data collection');
+});
+
+test('#cacheData stores a collection of resources', function(assert) {
+  let resourceA = Post.create({ id: '1' });
+  let resourceB = Post.create({ id: '2' });
+  subject.cacheData({ data: Ember.A([resourceA, resourceB]) });
+  assert.equal(subject.get('cache.data').get('firstObject'), resourceA, 'resourceA added to cache.data collection');
+  assert.equal(subject.get('cache.data').get('lastObject'), resourceB, 'resourceA added to cache.data collection');
+});
