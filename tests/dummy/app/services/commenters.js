79feb9572cb713fb359ecfb1dc85c168147e2830
Bill Heaton <pixelhandler@gmail.com>
2015-06-14 21:03:47 -0700

    Update to Ember 1.13.0 fixup deprecations in dummy app

--- tests/dummy/app/services/commenters.js
+++ tests/dummy/app/services/commenters.js
@@ -1,4 +1,6 @@
 import Adapter from '../adapters/commenter';
 import ServiceCache from 'ember-jsonapi-resources/mixins/service-cache';
 
+Adapter.reopenClass({ isServiceFactory: true });
+
 export default Adapter.extend(ServiceCache);
--- tests/dummy/app/services/comments.js
+++ tests/dummy/app/services/comments.js
@@ -1,4 +1,6 @@
 import Adapter from '../adapters/comment';
 import ServiceCache from 'ember-jsonapi-resources/mixins/service-cache';
 
+Adapter.reopenClass({ isServiceFactory: true });
+
 export default Adapter.extend(ServiceCache);
