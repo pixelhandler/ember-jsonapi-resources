5f5e0ce447cbef70b16fe67ebcb22a93eebc0d57
Bill Heaton <pixelhandler@gmail.com>
2015-09-23 10:30:29 -0700

    Fix ridiculous typo in Update Relationships method

--- addon/models/resource.js
+++ addon/models/resource.js
@@ -143,7 +143,7 @@ const Resource = Ember.Object.extend({
     } else {
       existing = this.get(relationshipData).map(function(rel) { return rel.id; });
       if (!existing.length) {
-        this.addRelationship(relation, ids);
+        this.addRelationships(relation, ids);
       } else if (ids.length > existing.length) {
         this.addRelationships(relation, unique(ids, existing));
       } else if (existing.length > ids.length) {
