f773b838175edaf5ba5213921d4a3e1d0632c5e0
Bill Heaton <pixelhandler@gmail.com>
2015-11-30 19:31:23 -0800

    Fix default blueprint so afterInstall adds bower and npm deps

--- blueprints/ember-jsonapi-resources/index.js
+++ blueprints/ember-jsonapi-resources/index.js
@@ -1,12 +1,26 @@
+var Promise = require('ember-cli/lib/ext/promise');
+
 module.exports = {
   description: 'ember-jsonapi-resources',
 
   normalizeEntityName: function () {},
 
   afterInstall: function () {
-    return this.addBowerPackagesToProject([
-      { name: 'fetch' },
-      { name: 'es6-promise' }
+    return Promise.all([
+      this.addBowerPackagesToProject([
+        { name: 'fetch' },
+        { name: 'es6-promise' }
+      ]),
+      this.addPackagesToProject([
+        { name: 'inflection', target: '~1.7.1' },
+        { name: 'lodash', target: '~3.10.1' },
+        { name: 'ember-inflector',target: '^1.6.2' },
+        { name: 'ember-cli-string-utils', target: '^1.0.0' },
+        { name: 'ember-cli-path-utils', target: '^1.0.0' },
+        { name: 'ember-cli-test-info', target: '^1.0.0' },
+        { name: 'ember-cli-get-dependency-depth', target: '^1.0.0' },
+        { name: 'silent-error', target: '^1.0.0' }
+      ])
     ]);
   }
 };
--- index.js
+++ index.js
@@ -13,18 +13,5 @@ module.exports = {
     });
 
     app.import(app.bowerDirectory + '/fetch/fetch.js');
-  },
-
-  afterInstall: function() {
-    return this.addPackagesToProject([
-      { name : 'inflection', version : '~1.7.1' },
-      { name : 'lodash', version : '~3.10.1' },
-      { name : 'ember-inflector', version : '^1.6.2' },
-      { name : 'ember-cli-string-utils', version: '^1.0.0' },
-      { name : 'ember-cli-path-utils', version: '^1.0.0' },
-      { name : 'ember-cli-test-info', version: '^1.0.0' },
-      { name : 'ember-cli-get-dependency-depth', version: '^1.0.0' },
-      { name : 'silent-error', version: '^1.0.0' }
-    ]);
   }
 };
