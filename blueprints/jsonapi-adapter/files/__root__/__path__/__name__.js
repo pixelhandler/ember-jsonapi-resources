ec02ce1f41b169fa72970a39e5797667fbd1d26a
Bill Heaton <pixelhandler@gmail.com>
2016-09-09 18:09:26 -0700

    Update comment for Adapter#url property/type

--- blueprints/jsonapi-adapter/files/__root__/__path__/__name__.js
+++ blueprints/jsonapi-adapter/files/__root__/__path__/__name__.js
@@ -4,17 +4,20 @@ import config from '../config/environment';
 export default <%= baseClass %>.extend({
   type: '<%= entity %>',
 
-  /*
-   * url: full url/path to API endpoint for this resource.
-   *
-   * This property is optional, as the application adapter provides a sane
-   * default combining config.APP.API_HOST, config.APP.API_PATH and type
-   * through Ember computed properties. This is faster however, and provides
-   * easy customization per adapter.
-   *
-   * Url is always fetched through #fetchUrl method to provide runtime
-   * manipulation of the url, either per adapter or application-wide on the
-   * application adapter. See AdapterApiHostProxyMixin for an example.
-   */
+  /**
+    Full url/path to API endpoint for this resource.
+
+    This property is optional, as the application adapter provides a sane
+    default combining config.APP.API_HOST, config.APP.API_PATH and type
+    through Ember computed properties. This is faster however, and provides
+    easy customization per adapter.
+
+    Url is always fetched through #fetchUrl method to provide runtime
+    manipulation of the url, either per adapter or application-wide on the
+    application adapter. See AdapterApiHostProxyMixin for an example.
+
+    @property url
+    @type String
+  */
   url: [config.APP.API_HOST, config.APP.API_PATH, '<%= resource %>'].join('/')
 });
