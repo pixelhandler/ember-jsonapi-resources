62b36992177378504e0bf05961225e537adb1df0
Bill Heaton <pixelhandler@gmail.com>
2016-12-22 01:26:41 -0800

    Conditional use of Ember.assign to support 2.4

--- tests/helpers/start-app.js
+++ tests/helpers/start-app.js
@@ -3,10 +3,15 @@ import Application from '../../app';
 import config from '../../config/environment';
 
 export default function startApp(attrs) {
-  let application;
+  let application, attributes;
 
   // use defaults, but you can override
-  let attributes = Ember.assign({}, config.APP, attrs);
+  if (Ember.assign) {
+    attributes = Ember.assign({}, config.APP, attrs);
+  } else {
+    attributes = Ember.merge({}, config.APP);
+    attributes = Ember.merge(attributes, attrs);
+  }
 
   Ember.run(() => {
     application = Application.create(attributes);
